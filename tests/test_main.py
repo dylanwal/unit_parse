9c9e4ecee5eba7b8735371a5063a45b874598dcb
Wicke, Yannik | Friedrich Lütze GmbH <yannik.wicke@luetze.de>
2024-05-14 10:38:00 +0200

    fix converting to units when magnitude is 1

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,6 @@
 import pytest
 
-from unit_parse import parser, Quantity
-
+from unit_parse import Quantity, parser
 
 examples = [
     # standard examples
@@ -65,6 +64,7 @@ examples = [
     ['20.8 mm Hg (25 °C)', [[Quantity('20.8 mmHg'), Quantity('25 degC')]]],
     ['20.8 mm Hg at 25 °C', [[Quantity('20.8 mmHg'), Quantity('25 degC')]]],
     ["-4,395.63 kJ/mol at 25 °C", [[Quantity('-4395.63 kJ/mol'), Quantity('25 degC')]]],
+    ["1.0A @ 25kW", [[Quantity('1.0A'), Quantity('25kW')]]],
 
     # list of quantities
     ['18 mm Hg; 20 mm Hg', Quantity('20 mmHg')],
